60c49e837a9cc60f3ae9cc5b4e7fef9e6263fca4
ossuchas <suchat.s14102526@gmail.com>
2019-08-10 07:15:28 +0700

    modify

--- src/app/layout/agreview-page/agreview-page.component.ts
+++ src/app/layout/agreview-page/agreview-page.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
   selector: 'app-agreview-page',
   templateUrl: './agreview-page.component.html',
-  styleUrls: ['./agreview-page.component.scss']
+  styleUrls: ['./agreview-page.component.scss'],
+    animations: [routerTransition()]
 })
 export class AgreviewPageComponent implements OnInit {
 
--- src/app/layout/agreview-page/agreview-page.module.ts
+++ src/app/layout/agreview-page/agreview-page.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';
 
 import { AgreviewPageRoutingModule } from './agreview-page-routing.module';
 import { AgreviewPageComponent } from './agreview-page.component';
-
+import { PageHeaderModule } from 'src/app/shared';
 
 @NgModule({
-    imports: [CommonModule, AgreviewPageRoutingModule],
+    imports: [CommonModule, AgreviewPageRoutingModule, PageHeaderModule],
     declarations: [AgreviewPageComponent]
 })
 export class AgreviewPageModule {}
