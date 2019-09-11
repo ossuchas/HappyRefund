6b4d54ac8b413d8928bd4b258b42db51b3382a0d
ossuchas <suchat.s14102526@gmail.com>
2019-09-12 04:22:34 +0700

    add new fileds for thankyou

--- src/app/shared/pipes/tf02docstatus.pipe.ts
+++ src/app/shared/pipes/tf02docstatus.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class Tf02docstatusPipe implements PipeTransform {
     transform(value: any, args?: any): any {
         if (value === 'N') {
-            return 'รอตรวจเอกสารtf02';
+            return 'รอตรวจยืนยันเอกสาร';
         } else if (value === 'A') {
-            return 'เอกสารผ่านtf02';
+            return 'เอกสารผ่านยืนยันแล้ว';
         } else if (value === 'R') {
-            return 'เอกสารไม่ผ่านtf02';
+            return 'เอกสารไม่ผ่าน';
         } else {
             return 'N/A';
         }
