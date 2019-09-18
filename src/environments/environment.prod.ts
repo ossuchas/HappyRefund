f097943159187d1329ab49eacfb61a1670f058f0
ossuchas <suchat.s14102526@gmail.com>
2019-09-18 19:15:19 +0700

    modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
 //   apiUrl: 'http://happyrefundapi-happyrefund.devops-app.apthai.com/api/v1',
   apiUrl: 'http://happyrefundapi-happyrefunds.devops-app.apthai.com/api/v1',
   memoUrl_1: 'http://www.ap-ir.com/WebSalesReport/Forms/WF_Print_Form_Viewer.aspx?PFID=\
-  PF_TR_009_2&PFName=PF_TR_009_2.rpt&ParaName=@TransferNumber&ParaValue=',
+  PF_TR_009_1&PFName=PF_TR_009_1.rpt&ParaName=@TransferNumber&ParaValue=',
   memoUrl_2: '&ExtraQueryString=%7C@NitiBankName*%7C@NitiBankType*1%7C@NitiBankNo*%7C@\
   CustomerBankName*%7C@CustomerBankType*1%7C@CustomerBankNo*%7C@ContactID*'
 };
