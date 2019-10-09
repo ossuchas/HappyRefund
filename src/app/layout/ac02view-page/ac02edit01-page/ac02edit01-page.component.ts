21844fa4298d0eca1d33f2cb0ebacc7fcda3671d
ossuchas <suchat.s14102526@gmail.com>
2019-10-09 16:16:55 +0700

    comment due date ac02 page

--- src/app/layout/ac02view-page/ac02edit01-page/ac02edit01-page.component.ts
+++ src/app/layout/ac02view-page/ac02edit01-page/ac02edit01-page.component.ts
@@ -33,22 +33,32 @@ export class Ac02edit01PageComponent implements OnInit {
         // console.log('Current User: ' + this.currentUser.userPrincipalName);
         console.log('Submit hyrf_id = ' + form.value.hyrf_id);
         if (form.value.ac02_appv_flag === 'A') {
-            if (form.value.ac02_due_date == null) {
-                this.snackBar.open('Please choose Due Date to refund..!!!', '', {
+            // if (form.value.ac02_due_date == null) {
+            //     this.snackBar.open('Please choose Due Date to refund..!!!', '', {
+            //         duration: 3000
+            //     });
+            // } else {
+            //     form.value.ac02_appv_by = this.currentUser.userPrincipalName;
+            //     console.log(form.value);
+            //     this.service.updateAC02Status(form.value).subscribe(res => {
+            //         this.snackBar.open('Updated transaction Successful...!! [' + res.hyrf_id + ']', '', {
+            //             duration: 3000
+            //         });
+            //     });
+
+            //     this.dialogbox.close();
+            //     this.service.filter('Update click');
+            // }
+            form.value.ac02_appv_by = this.currentUser.userPrincipalName;
+            console.log(form.value);
+            this.service.updateAC02Status(form.value).subscribe(res => {
+                this.snackBar.open('Updated transaction Successful...!! [' + res.hyrf_id + ']', '', {
                     duration: 3000
                 });
-            } else {
-                form.value.ac02_appv_by = this.currentUser.userPrincipalName;
-                console.log(form.value);
-                this.service.updateAC02Status(form.value).subscribe(res => {
-                    this.snackBar.open('Updated transaction Successful...!! [' + res.hyrf_id + ']', '', {
-                        duration: 3000
-                    });
-                });
+            });
 
-                this.dialogbox.close();
-                this.service.filter('Update click');
-            }
+            this.dialogbox.close();
+            this.service.filter('Update click');
         } else {
             form.value.ac02_appv_by = this.currentUser.userPrincipalName;
             this.service.updateAC02Reject(form.value).subscribe(res => {
